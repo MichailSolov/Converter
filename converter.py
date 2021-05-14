3274d40fb1d00f06f32686085d8430591ae57af1
MichailSolov <56672359+MichailSolov@users.noreply.github.com>
2021-05-15 00:42:09 +0300

    Delete converter.py

--- converter.py
+++
@@ -1,9 +0,0 @@
-    if isinstance(num, str):
-        n = int(num, from_base)
-    else:
-        n = int(num)
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if n < to_base:
-        return alphabet[n]
-    else:
-        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
