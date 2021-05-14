856502330668eff90ead4d241c5e37836ef6f606
MichailSolov <56672359+MichailSolov@users.noreply.github.com>
2021-05-14 23:16:00 +0300

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,9 @@
+    if isinstance(num, str):
+        n = int(num, from_base)
+    else:
+        n = int(num)
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < to_base:
+        return alphabet[n]
+    else:
+        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
